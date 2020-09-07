ca44cfad35aa3eeeef8ad1c2920866948c0dce83
Lagrato <45488845+Lagrato@users.noreply.github.com>
2020-09-07 14:46:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,23 @@
 window.onload = () => {
     const button = document.getElementById('bu');
     const classLabel = document.getElementById('class');
-    const classList = ['Scout','Hunter','Archer','Musketeer','Sniper','Blaster','Mercenary','Gunner',
-    'Warrior','Destroyer','Spearman','Ravager','Samurai','Fighter','Slayer','Berserker',
-    'Mage','Grenadier','Astronaut','Bomber','Illusionist','Necromancer','Wizard',
-    'Thief','Ninja','Stalker','Trickster','Gambler','Hitman','Recon',
-    'Priest','Bishop','Alchemist','Controller','Programmer','Medic',
-    'Defender','Guardian','Monk','Admiral','Juggernaut','Paladin','Knight',
-    'Phantom','Jester','Cowboy','Turkey','Snowman','Santa','Firecracker','Wind Dancer','Gladiator','Bunny','Frost',
-    'Fairy','Chrono','Shroom','Rocketeer','Demon','Elementalist','Valkyrie','Duelist','Ghost','Beast','Reaper'];
+
+    const marksman = {color: ['#4DA6FF','#323232'], classes: ['Scout','Hunter','Archer','Musketeer','Sniper','Blaster','Mercenary','Gunner']};
+    const brawler = {color: ['#FFB319','#323232'], classes: ['Warrior','Destroyer','Spearman','Ravager','Samurai','Fighter','Slayer','Berserker']};
+    const burst = {color: ['#8C19FF','#E7E7E7'], classes: ['Mage','Grenadier','Astronaut','Bomber','Illusionist','Necromancer','Wizard']};
+    const assassin = {color: ['#FF4D4D','#323232'], classes: ['Thief','Ninja','Stalker','Trickster','Gambler','Hitman','Recon']};
+    const support = {color: ['#E6E600','#323232'], classes: ['Priest','Bishop','Alchemist','Controller','Programmer','Medic']};
+    const tank = {color: ['#008000','#E7E7E7'], classes: ['Defender','Guardian','Monk','Admiral','Juggernaut','Paladin','Knight']};
+    const special = {color: ['#0000DC','#E7E7E7'], classes: ['Phantom','Jester','Cowboy','Turkey','Snowman','Santa','Firecracker','Wind Dancer','Gladiator','Bunny','Frost']};
+    const gamepass = {color: ['#D2D2D2','#323232'], classes: ['Fairy','Chrono','Shroom','Rocketeer','Demon','Elementalist','Valkyrie','Duelist','Ghost','Beast','Reaper']};
+
+    const classList = [marksman,brawler,burst,assassin,support,tank,special,gamepass];
 
     button.onclick = () => {
-        classLabel.textContent = classList[randomIntFromInterval(0,classList.length-1)];
+        let selectedCategory = classList[randomIntFromInterval(0,classList.length-1)];
+        classLabel.textContent = selectedCategory.classes[randomIntFromInterval(0,selectedCategory.classes.length-1)];
+        classLabel.style.backgroundColor = selectedCategory.color[0];
+        classLabel.style.color = selectedCategory.color[1];
     };
 }
 const randomIntFromInterval = (min, max) => { return Math.floor(Math.random() * (max - min + 1) + min) };
