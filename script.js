d7a3d1a4937b06bbc1b467c15634663c09af900d
Lagrato <45488845+Lagrato@users.noreply.github.com>
2020-12-23 21:26:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ window.onload = () => {
     const button = document.getElementById('bu');
     const classLabel = document.getElementById('class');
 
-    const marksman = {color: ['#4DA6FF','#323232'], classes: ['Scout','Hunter','Archer','Musketeer','Sniper','Blaster','Mercenary','Gunner','Ranger']};
-    const brawler = {color: ['#FF6619','#323232'], classes: ['Warrior','Destroyer','Spearman','Ravager','Samurai','Fighter','Slayer','Berserker','Charger']};
-    const burst = {color: ['#8C19FF','#E7E7E7'], classes: ['Mage','Grenadier','Astronaut','Bomber','Illusionist','Necromancer','Wizard','Druid','Sorcerer']};
-    const assassin = {color: ['#FF4D4D','#323232'], classes: ['Thief','Ninja','Stalker','Trickster','Gambler','Hitman','Recon','Dancer']};
-    const support = {color: ['#E6E600','#323232'], classes: ['Priest','Bishop','Alchemist','Controller','Programmer','Medic']};
-    const tank = {color: ['#008000','#E7E7E7'], classes: ['Defender','Guardian','Monk','Admiral','Juggernaut','Paladin','Knight','Ruler']};
-    const special = {color: ['#0000DC','#E7E7E7'], classes: ['Phantom','Jester','Cowboy','Turkey','Snowman','Santa','Firecracker','Wind Dancer','Gladiator','Bunny','Frost']};
+    const marksman = {color: ['hsl(210, 30%, 65%)','#323232'], classes: ['Scout','Hunter','Archer','Musketeer','Sniper','Blaster','Mercenary','Gunner']};
+    const brawler = {color: ['hsl(40,70%,55%)','#323232'], classes: ['Warrior','Destroyer','Spearman','Ravager','Samurai','Fighter','Slayer','Berserker']};
+    const burst = {color: ['hsl(270,30%,55%)','#E7E7E7'], classes: ['Mage','Grenadier','Astronaut','Bomber','Illusionist','Necromancer','Wizard']};
+    const assassin = {color: ['hsl(0,50%,65%)','#323232'], classes: ['Thief','Ninja','Stalker','Trickster','Gambler','Hitman','Recon']};
+    const support = {color: ['hsl(60, 40%, 45%)','#323232'], classes: ['Priest','Bishop','Alchemist','Controller','Programmer','Medic']};
+    const tank = {color: ['hsl(120, 40%, 25%)','#E7E7E7'], classes: ['Defender','Guardian','Monk','Admiral','Juggernaut','Paladin','Knight']};
+    const special = {color: ['hsl(240, 60%, 43%)','#E7E7E7'], classes: ['Phantom','Jester','Cowboy','Turkey','Snowman','Santa','Firecracker','Wind Dancer','Gladiator','Bunny','Frost']};
     const gamepass = {color: ['#D2D2D2','#323232'], classes: ['Fairy','Chrono','Shroom','Rocketeer','Demon','Elementalist','Valkyrie','Duelist','Ghost','Beast','Reaper']};
 
     const classList = [marksman,brawler,burst,assassin,support,tank,special,gamepass];
 
     button.onclick = () => {
-        let selectedCategory = classList[randomIntFromInterval(0,classList.length-1)];
-        classLabel.textContent = selectedCategory.classes[randomIntFromInterval(0,selectedCategory.classes.length-1)];
+        const selectedCategory = classList[randomIntFromInterval(0,classList.length-1)];
+        classLabel.children[0].textContent = selectedCategory.classes[randomIntFromInterval(0,selectedCategory.classes.length-1)];
         classLabel.style.backgroundColor = selectedCategory.color[0];
         classLabel.style.color = selectedCategory.color[1];
     };
